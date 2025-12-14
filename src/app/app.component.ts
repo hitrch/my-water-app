067c0dc68749e348041ed48591017c0474647b48
hitrch <vladislawpaliy@gmail.com>
2025-12-14 17:47:36 +0200

    project inited & tailwindcss added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-water-app';
